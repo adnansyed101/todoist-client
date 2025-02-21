02108393a13d8c75f38acbca42e9bdf3fae1b4b2
Adnan <sirgeant10@gmail.com>
2025-02-21 19:09:41 +0600

    Add homepage route.

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
+import SignIn from "../pages/Signin";
+import SignUp from "../pages/SignUp";
+import HomePage from "../pages/HomePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+    ],
   },
 ]);
 
-export default router
+export default router;
