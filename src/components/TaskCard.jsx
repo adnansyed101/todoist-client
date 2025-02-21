706b371f0c6699693d2b8d2f077134640e540b86
Adnan <sirgeant10@gmail.com>
2025-02-21 23:05:35 +0600

    Create components

---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,33 @@
+import { useDraggable } from "@dnd-kit/core";
+import PropTypes from "prop-types";
+
+const TaskCard = ({ task }) => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: task.id,
+  });
+
+  const style = transform
+    ? {
+        transform: `translate(${transform.x}px, ${transform.y}px)`,
+      }
+    : undefined;
+
+  return (
+    <div
+      ref={setNodeRef}
+      {...listeners}
+      {...attributes}
+      className="cursor-grab rounded-lg bg-neutral-700 p-4 shadow-sm hover:shadow-md"
+      style={style}
+    >
+      <h3 className="font-medium text-neutral-100">{task.title}</h3>
+      <p className="mt-2 text-sm text-neutral-400">{task.description}</p>
+    </div>
+  );
+};
+
+TaskCard.propTypes = {
+  task: PropTypes.object,
+};
+
+export default TaskCard;
