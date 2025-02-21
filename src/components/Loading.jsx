cad2c9bc80b7d6c0cf9c7d8b189dfd648171a664
Adnan <sirgeant10@gmail.com>
2025-02-21 19:08:39 +0600

    Create loading comp

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <span className="loading loading-spinner text-accent"></span>
+    </div>
+  );
+};
+
+export default Loading;
