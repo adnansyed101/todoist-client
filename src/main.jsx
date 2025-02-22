40cafecf358dc6c852d6f6add29b81d71382edac
Adnan <sirgeant10@gmail.com>
2025-02-22 13:41:38 +0600

    Add queryclient

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,16 @@ import AuthProvider from "./provider/AuthProvider.jsx";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import router from "./routes/router.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
       <ToastContainer position="top-left" />
     </AuthProvider>
   </StrictMode>
