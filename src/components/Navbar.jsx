5930f5a135417fdb8634bbf6105c902a9af044e6
Adnan <sirgeant10@gmail.com>
2025-02-22 23:30:57 +0600

    Remove navbar options.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,8 @@
 import { Link, NavLink } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
-import { useEffect } from "react";
-
-import useAuth from "../hooks/useAuth";
+import NavbarOptions from "./NavbarOptions";
 
 const Navbar = () => {
-  const { user, logOut, theme, toggleTheme } = useAuth();
-
-  useEffect(() => {
-    document.querySelector("html").setAttribute("data-theme", theme);
-  }, [theme]);
-
   const links = (
     <>
       <li>
@@ -25,50 +17,6 @@ const Navbar = () => {
       </li>
     </>
   );
-
-  const navOptions =
-    user && user?.email ? (
-      <div className="dropdown dropdown-end">
-        <div
-          tabIndex={0}
-          role="button"
-          className="btn btn-ghost btn-circle avatar"
-        >
-          <div className="w-10 rounded-full">
-            <img
-              alt="Photo"
-              src={user?.photoURL}
-              referrerPolicy="no-referrer"
-            />
-          </div>
-        </div>
-        <ul
-          tabIndex={0}
-          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-        >
-          <li className="text-center mb-2">{user?.displayName}</li>
-          <li className="mb-2">
-            <button onClick={toggleTheme} className="btn btn-primary btn-sm">
-              {theme === "light" ? "light" : "dark"}
-            </button>
-          </li>
-          <li>
-            <button onClick={logOut} className="btn btn-warning btn-sm">
-              Logout
-            </button>
-          </li>
-        </ul>
-      </div>
-    ) : (
-      <div className="flex flex-col md:flex-row gap-1">
-        <Link to="/signin" className="btn btn-xs md:btn-md btn-accent">
-          Sign In
-        </Link>
-        <Link to="/signup" className="btn btn-xs md:btn-md  btn-secondary">
-          Sign Up
-        </Link>
-      </div>
-    );
 
   return (
     <header className="w-full fixed top-0 z-50 bg-base-100 border-b">
@@ -92,7 +40,9 @@ const Navbar = () => {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1 flex gap-2">{links}</ul>
         </div>
-        <div className="navbar-end space-x-2">{navOptions}</div>
+        <div className="navbar-end space-x-2">
+          <NavbarOptions />
+        </div>
       </div>
     </header>
   );
