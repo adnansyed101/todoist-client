4fc8663ed726bbc8dfd7c3fc3b9bb922bf60ca32
Adnan <sirgeant10@gmail.com>
2025-02-22 20:42:09 +0600

    Update homepage to show hero.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,76 +1,21 @@
-import { toast } from "react-toastify";
-import { useEffect, useState } from "react";
-import useAxiosPublic from "../hooks/useAxiosPublic";
-import useAuth from "../hooks/useAuth";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  const queryClient = useQueryClient();
-  const [tasks, setTasks] = useState([]);
-  const axiosPublic = useAxiosPublic();
-  const { user } = useAuth();
-
-  const { data, status } = useQuery({
-    queryKey: ["allTasks", user?.uid],
-    queryFn: async () => {
-      const { data } = await axiosPublic.get(`/task?fireId=${user?.uid}`);
-      return data;
-    },
-  });
-
-  const { mutate: createTask } = useMutation({
-    mutationFn: (newTask) => axiosPublic.post("/task", newTask),
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["allTasks"] }),
-  });
-
-  useEffect(() => {
-    if (status === "success") {
-      setTasks(data);
-    }
-  }, [status, data]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-
-    const title = form.title.value;
-    const description = form.description.value;
-
-    const taskData = {
-      title,
-      description,
-      status: "To Do",
-      position: tasks.length + 1,
-      fireId: user.uid,
-    };
-
-    try {
-      createTask(taskData);
-      toast.success("Task Created");
-    } catch (err) {
-      toast.err(err.message);
-    }
-  };
-
   return (
-    <div className="py-20">
-      <form onSubmit={handleSubmit} className="space-x-2 ml-2">
-        <input
-          type="text"
-          name="title"
-          className="input input-bordered input-primary"
-          placeholder="Title"
-        />
-        <input
-          type="text"
-          name="description"
-          className="input input-bordered input-primary"
-          placeholder="Description"
-        />
-        <button className="btn btn-primary" type="submit">
-          Add Task
-        </button>
-      </form>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <Link to={"/dashboard/addTask"} className="btn btn-primary">
+            Get Started
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
