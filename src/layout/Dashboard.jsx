06dfe7a4e6e4bddff81335f4de76612605705c6b
Adnan <sirgeant10@gmail.com>
2025-02-22 23:31:39 +0600

    Fix sidebar width.

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,21 +1,16 @@
 import { Link, Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import DashNav from "../components/DashNav";
 
 const Dashboard = () => {
   return (
     <>
-      <Navbar />
+      <DashNav />
       <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
           {/* Page content here */}
           <Outlet />
-          <label
-            htmlFor="my-drawer-2"
-            className="btn btn-primary drawer-button lg:hidden"
-          >
-            Open drawer
-          </label>
+          
         </div>
         <div className="drawer-side">
           <label
@@ -23,7 +18,7 @@ const Dashboard = () => {
             aria-label="close sidebar"
             className="drawer-overlay"
           ></label>
-          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          <ul className="menu bg-base-200 text-base-content min-h-full w-60 p-4">
             {/* Sidebar content here */}
             <li>
               <Link to={"/dashboard/addTask"}>Add Task</Link>
